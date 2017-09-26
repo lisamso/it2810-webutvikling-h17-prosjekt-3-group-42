06600699b5f422d861049a557804c9f72ff6dffa
Tollef Jørgensen <tollefj@gmail.com>
2017-09-27 01:21:50 +0200

    ESLint integration

--- pimp/src/components/Home.js
+++ pimp/src/components/Home.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types'; // ES6
+//import PropTypes from 'prop-types'; // ES6
 
-const formatName = user => `${user.firstName} ${user.lastName}`;
+const pageTitle = page => `Welcome to ${page.title}`;
 
-const user = {
-  firstName: 'React',
-  lastName: 'Boilerplate'
+const page = {
+  title: 'PIMp',
+  subTitle: 'Personal Information Manager',
 };
 
-class Home extends Component{
-  render() {
-    return (
+class Home extends Component {
+	render() {
+		return (
 			<div>
-				<h1>{formatName(user)}!</h1>
-				<h2>(With SCSS!)</h2>
+				<h1>{pageTitle(page)}!</h1>
+				<h3>{page.subTitle}</h3>
 			</div>
 		)
-  }
-};
+	}
+}
 
 export default Home;
--- pimp/webpack.config.js
+++ pimp/webpack.config.js
@@ -7,51 +7,56 @@ const port = process.env.PORT || 3000;
 const outputPath = path.join(__dirname, "dist")
 
 module.exports = {
-    entry: './src/App.js',
-    output: {
-        path: __dirname,
-        filename: 'dist/bundle.js'
-    },
-    resolve: {
-        modules: ['node_modules', './src'],
-        extensions: ['.js', '.jsx'],
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.scss$/,
-                use: ExtractTextPlugin.extract({
-                    use: 'css-loader!sass-loader'
-                }),
-            },
-            {
-                test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                    use: 'css-loader'
-                }),
-            },
-            {
-                test: /\.(js|jsx)$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/, 
-                query: {
-                    presets:['es2015','react']
-                }
-            }
-        ]
-    },
-    plugins: [
-        new ExtractTextPlugin("bundle.css"),
-        new webpack.HotModuleReplacementPlugin(),
-        new HtmlWebpackPlugin({
-            inject: true,
-            template: path.join(__dirname, '/index.html')
-        }),
-        new webpack.EnvironmentPlugin({ NODE_ENV: 'development' }),
-    ],
-    devtool: "source-map",
-    devServer: {
-        port,
-        hot: true
-    }
+	entry: './src/App.js',
+	output: {
+		path: __dirname,
+		filename: 'dist/bundle.js'
+	},
+	resolve: {
+		modules: ['node_modules', './src'],
+		extensions: ['.js', '.jsx'],
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.scss$/,
+				use: ExtractTextPlugin.extract({
+						use: 'css-loader!sass-loader'
+				}),
+			},
+			{
+				test: /\.css$/,
+				use: ExtractTextPlugin.extract({
+						use: 'css-loader'
+				}),
+			},
+			{
+				test: /\.(js|jsx)$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/, 
+				query: {
+					presets:['es2015','react']
+				}
+			},
+			{
+				test: /\.(js|jsx)$/,
+				loader: 'eslint-loader',
+				exclude: /node_modules/, 
+			},
+		]
+	},
+	plugins: [
+		new ExtractTextPlugin("bundle.css"),
+		new webpack.HotModuleReplacementPlugin(),
+		new HtmlWebpackPlugin({
+			inject: true,
+			template: path.join(__dirname, '/index.html')
+		}),
+		new webpack.EnvironmentPlugin({ NODE_ENV: 'development' }),
+	],
+	devtool: "source-map",
+	devServer: {
+		port,
+		hot: true
+	}
 }
