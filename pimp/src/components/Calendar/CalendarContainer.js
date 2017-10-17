e39b453168713e24a9c946dbeea8267122005392
Tollef Jørgensen <tollefj@gmail.com>
2017-10-17 13:37:25 +0200

    minor style changes

--- pimp/src/components/Calendar/CalendarContainer.js
+++ pimp/src/components/Calendar/CalendarContainer.js
@@ -12,23 +12,28 @@ class CalendarContainer extends Component {
   // Makes a specific date blue
   // Can use this to mark event dates
   // The date and month must be integers
-  addEventOnDay(date) {
+  addEventOnDay(date, eventHover) {
     const buttons = document.getElementsByClassName(
       "react-calendar__month-view__days__day");
     for (let i = 0; i < buttons.length; i++) {
+      let btn = buttons[i]
       let btndate
         = new Date(buttons[i].firstChild.attributes.item(0).textContent);
       if (btndate.getDate() === date.getDate() &&
           btndate.getMonth() === date.getMonth()) {
-        buttons[i].style.background = "#2ecc71";
-        buttons[i].style.color = "black";
+        btn.style.background = "#2ecc71";
+        btn.style.color = "black";
+        btn.title = eventHover;
+        btn.className += " tile-has-event"
+
       }
     }
   }
   
   componentWillUpdate() {
     JSON.parse(localStorage.getItem('events')).map((event)=>{
-      this.addEventOnDay(new Date(Date.parse(event['eventAt'])));
+      let eventHover = `${event['title']}: ${event['text']}`
+      this.addEventOnDay(new Date(Date.parse(event['eventAt'])), eventHover);
     })
   }
   
