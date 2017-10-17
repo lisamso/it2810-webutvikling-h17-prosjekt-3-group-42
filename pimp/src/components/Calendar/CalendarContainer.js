845b6797f3992c3a7a4fb0ebbb801ab1c9d409d5
Maria Soleim <MariaSoleim@users.noreply.github.com>
2017-10-17 15:28:17 +0200

    Events can be deleted from calendar (#21)
    * Weekend days also get black text when having an event. Events is deleted from calendar when deleted from events.
    
    * Only showing events in current month in the calendar.
    
    * Refactoring, commenting and code cleanup.

--- pimp/src/components/Calendar/CalendarContainer.js
+++ pimp/src/components/Calendar/CalendarContainer.js
@@ -5,32 +5,54 @@ import Calendar from 'react-calendar/build/entry.nostyle';
 class CalendarContainer extends Component {
   constructor(props) {
     super(props);
-    
-    this.addEventOnDay = this.addEventOnDay.bind(this);
   }
   
-  // Makes a specific date blue
+  // Makes a specific date green
   // Can use this to mark event dates
   // The date and month must be integers
   addEventOnDay(date, eventHover) {
     const buttons = document.getElementsByClassName(
       "react-calendar__month-view__days__day");
+    const currentMonth = new Date().getMonth();
     for (let i = 0; i < buttons.length; i++) {
       let btn = buttons[i]
-      let btndate
-        = new Date(buttons[i].firstChild.attributes.item(0).textContent);
-      if (btndate.getDate() === date.getDate() &&
-          btndate.getMonth() === date.getMonth()) {
+      const btnDate = new Date(
+        btn.firstChild.attributes.item(0).textContent);
+      if (btnDate.getDate() === date.getDate() &&
+          btnDate.getMonth() === date.getMonth() &&
+          btnDate.getMonth() === currentMonth) {
         btn.style.background = "#2ecc71";
         btn.style.color = "black";
         btn.title = eventHover;
         btn.className += " tile-has-event"
-
       }
     }
   }
   
+  // Makes all the days transparent
+  deleteEvents() {
+    const buttons = document.getElementsByClassName(
+      "react-calendar__month-view__days__day");
+    for (let i = 0; i < buttons.length; i++) {
+      buttons[i].style.background = "transparent";
+      const date = new Date(
+        buttons[i].firstChild.attributes.item(0).textContent);
+      // If saturday or sunday, make text emerald
+      if (date.getDay() === 6 || date.getDay() === 0) {
+        buttons[i].style.color = "#2ecc71";
+      }
+      // Else make text white
+      else {
+        buttons[i].style.color = "white";
+      }
+    }
+  }
+  
+  // Runs every time the EventContainer updates
+  // Deleting makes all the days transparent before it goes through
+  // the events and makes any day having an event green.
   componentWillUpdate() {
+    this.deleteEvents();
     JSON.parse(localStorage.getItem('events')).map((event)=>{
       let eventHover = `${event['title']}: ${event['text']}`
       this.addEventOnDay(new Date(Date.parse(event['eventAt'])), eventHover);
